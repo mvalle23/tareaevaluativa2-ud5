ae17a0d78cb568d38c10199c4d41906e6d69eb70
bmadina <bmadina@birt.eus>
2021-11-23 15:14:25 +0100

    Main controller

--- src/main/java/eus/birt/dam/controller/MainController.java
+++ src/main/java/eus/birt/dam/controller/MainController.java
@@ -1,15 +1,23 @@
 package eus.birt.dam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import eus.birt.dam.repository.CyclistRepository;
 
 
 @Controller
 	public class MainController {
-			@GetMapping ({"/","/welcome"})
-			public String welcome() {
-				return "index";
-			}
+	
+	@Autowired
+   	private CyclistRepository cyclistRepository;
+	@GetMapping ({"/","/welcome"})
+	public String welcome(Model model) {
+		model.addAttribute("cyclists", cyclistRepository.findAll());
+		return "index";
+	}
 }
 	
 
