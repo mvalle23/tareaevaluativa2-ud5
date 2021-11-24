14cbb15918922336a89d7319d15038d6800ec4d4
bmadina <bmadina@birt.eus>
2021-11-24 13:00:15 +0100

    controller changes and new templates

--- src/main/java/eus/birt/dam/controller/MainController.java
+++ src/main/java/eus/birt/dam/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import eus.birt.dam.repository.CyclistRepository;
+import eus.birt.dam.repository.TeamRepository;
 
 
 @Controller
@@ -13,11 +14,24 @@ import eus.birt.dam.repository.CyclistRepository;
 	
 	@Autowired
    	private CyclistRepository cyclistRepository;
+	@Autowired
+   	private TeamRepository teamRepository;
 	
 	@GetMapping ({"/","/welcome"})
-	public String welcome(Model model) {
-		model.addAttribute("cyclists", cyclistRepository.findAll());
+	public String welcome() {
 		return "index";
+	}
+	
+	@GetMapping ({"/cyclists"})
+	public String getCyclists(Model model) {
+		model.addAttribute("cyclists", cyclistRepository.findAll());
+		return "cyclists";
+	}
+	
+	@GetMapping ({"/teams"})
+	public String getTeams(Model model) {
+		model.addAttribute("teams", teamRepository.findAll());
+		return "teams";
 	}
 }
 	
