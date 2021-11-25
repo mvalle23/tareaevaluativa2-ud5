308be8deb819ae5edba708748dcb2ad770e3b2d2
bmadina <bmadina@birt.eus>
2021-11-25 13:06:42 +0100

    Modal

--- src/main/java/eus/birt/dam/controller/CyclistController.java
+++ src/main/java/eus/birt/dam/controller/CyclistController.java
@@ -4,20 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import eus.birt.dam.service.CyclistService;
 
 @Controller
+@RequestMapping ("/cyclist")
 public class CyclistController {
 
 	@Autowired
 	CyclistService cyclistService;
 	
 	
-	@GetMapping("/cyclist/delete/{id}")
+	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable("id") Long id) {
-		
-		
 		cyclistService.deleteById(id);
 		return "redirect:/cyclists";
 	}
