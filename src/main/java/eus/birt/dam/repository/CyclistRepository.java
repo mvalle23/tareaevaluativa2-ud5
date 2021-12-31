85b6e1cbb2adba1a4b28140fd7836391b1b811a9
bmadina <bmadina@birt.eus>
2021-12-31 01:03:40 +0100

    cyclistRepository

--- src/main/java/eus/birt/dam/repository/CyclistRepository.java
+++ src/main/java/eus/birt/dam/repository/CyclistRepository.java
@@ -1,11 +1,6 @@
 package eus.birt.dam.repository;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import eus.birt.dam.domain.Cyclist;
 
